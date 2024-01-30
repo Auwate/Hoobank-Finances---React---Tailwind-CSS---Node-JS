1cdb0c1f32bb4729d48f27ce9d90f100be065f0b
Auwate <auwate1@gmail.com>
2024-01-30 16:38:59 -0500

    Implemented mobile support for navbar

--- modern-app/bank_modern_app/src/components/Navbar.jsx
+++ modern-app/bank_modern_app/src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import {close, logo, menu} from '../assets/index';
 import {navLinks} from '../constants/index'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       
@@ -26,6 +29,37 @@ const Navbar = () => {
         ))}
 
       </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+          src={toggle ? close : menu}
+          alt="menu"
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => {setToggle((prev) => {return !prev})}}
+          />
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 
+          right-0 mx-4 my-2 min-w-[110px] rounded-xl sidebar`}>
+            
+            <ul className='list-none flex flex-col 
+      justify-end items-center flex-1'>
+
+              {navLinks.map((nav, index) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-normal cursor-pointer
+                  text-[16px] text-white ${index === navLinks.length-1 
+                    ? `mb-0` : `mb-16`}`}
+                >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+                </li>
+              ))}
+
+            </ul>
+
+          </div>
+      </div>
 
     </nav>
   )
