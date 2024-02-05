3a55160d40b26d11d5307a85aeba24aee62ca8ef
Auwate <auwate1@gmail.com>
2024-02-04 20:45:25 -0500

    worked on hero

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -1,14 +1,35 @@
 import styles from '../style'
 import {discount, robot} from '../assets'
+import GetStarted from './GetStarted'
 
 const Hero = () => (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col
       xl:px-0 sm:px-16 px-6`}>
+        
         <div className='flex flex-row items-center py-[6px]
         px-4 bg-discount-gradient rounded-[10px] mb-2'>
           <img src={discount} alt="discount" className='w-[32px] h-[32px]'></img>
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className='text-white'>20%</span> DISCOUNT FOR {" "}
+            <span className='text-white'>1 MONTH </span> {" "} ACCOUNT
+          </p>
         </div>
+
+        <div className='flex flex-row justify-between items-center w-full'>
+          <h1 className='flex-1 text-white 
+          font-poppins font-semibold
+          ss:text-[72px] text-[48px]'>
+            The Next <br className='sm:block hidden' /> {" "}
+            <span className='text-gradient'>Generation</span> <br/>
+            Payment Method
+          </h1>
+        </div>
+
+        <div className='sm:flex hidden md:mr-4 mr-0'>
+          <GetStarted/>
+        </div>
+
       </div>
     </section>
 )
